8fb63ad7f63ec3ac81938b25ae0b4ade63491bdd
Christoph Görn <goern@b4mad.net>
2016-01-13 10:16:19 +0100

    removed vmware provider

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,4 +5,4 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum install -y openssh-server
 yum update -y
 
-echo "OpenShift Origin - CentOS 7" > /etc/motd
+echo "OpenShift Origin 1.1 - CentOS 7" > /etc/motd
